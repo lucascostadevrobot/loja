423e7cccc7a7536194d159010d3f254dd8bfc2af
lucascostadeviff <lucascostadev@gmail.com>
2022-05-27 14:48:56 -0300

    v1.01 Incial commit projeto Loja Virtual
    Enviando o primeiro commit inicial do projeto.
    
    -Classe ControllerPrincipal
    -Dependencias injetadas
    -resources layout inserido

---
+++ src/main/java/com/euonline/virtual/controller/PrincipalController.java
@@ -0,0 +1,12 @@
+package com.euonline.virtual.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+    @GetMapping("/administrativo")
+    public String acessarPrincipal(){
+        return "administrativo/home"; //redireciona para  página home
+    }
+}
